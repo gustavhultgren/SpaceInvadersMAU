44e6dbbfca579061528595b269fbf7f29190956f
Gustav <hultgrengustav@gmail.com>
2019-04-03 11:12:09 +0200

    loop added

--- SpaceInvadersMAU/src/entity/Gustav.java
+++ SpaceInvadersMAU/src/entity/Gustav.java
@@ -4,6 +4,10 @@ public class Gustav {
 	
 	public static void main(String[] args) {
 		System.out.println("Jag heter Gustav");
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Tom heter inte Gustav");
+		}
 	}
 
 }
