020608ad695eef3c80f87b3f2032ceaab9d7d91b
Tom <Tom@Toms-MacBook-Air.local>
2019-04-03 17:30:57 +0200

    Added shell to serverclient class

--- SpaceInvadersMAU/src/server/ServerClient.java
+++ SpaceInvadersMAU/src/server/ServerClient.java
@@ -1,5 +1,28 @@
 package server;
 
-public class ServerClient {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class ServerClient extends Thread{
+	private int port;
+	
+	private ServerSocket serverSocket;
+	
+	public ServerClient(int port) {
+		this.port = port;
+		
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+		}
+		start();
+	}
+	
+	public void run() {
+		
+	}
+	
+	private class Worker {
+		
+	}
 }
