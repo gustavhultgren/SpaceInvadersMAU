ef5fc7cae270f2831fd5d32a266f519ea3c473d0
Tom <Tom@Toms-MacBook-Air.local>
2019-04-04 19:03:42 +0200

    Added changes to LeaderboardUpdateRespone class

--- SpaceInvadersMAU/src/server/LeaderboardUpdateResponse.java
+++ SpaceInvadersMAU/src/server/LeaderboardUpdateResponse.java
@@ -1,8 +1,9 @@
 package server;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class LeaderboardUpdateResponse {
+public class LeaderboardUpdateResponse implements Serializable{
 	private ArrayList<PlayerScore> scoreList;
 	
 	public LeaderboardUpdateResponse(ArrayList scoreList) {
