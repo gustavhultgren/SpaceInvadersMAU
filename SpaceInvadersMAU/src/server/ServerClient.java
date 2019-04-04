eea4c85a1199f4577d54739c6ced8f2fefed0117
Tom <Tom@Toms-MacBook-Air.local>
2019-04-04 11:10:08 +0200

    Added threadpool execution elelements and intergrated them in ServerClient

--- SpaceInvadersMAU/src/server/ServerClient.java
+++ SpaceInvadersMAU/src/server/ServerClient.java
@@ -1,28 +1,63 @@
 package server;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
-public class ServerClient extends Thread{
-	private int port;
-	
+public class ServerClient {
+	private Connection connection = new Connection();
 	private ServerSocket serverSocket;
-	
-	public ServerClient(int port) {
-		this.port = port;
-		
-		try {
-			serverSocket = new ServerSocket(port);
-		} catch (IOException e) {
+	private RunOnThreadN pool;
+
+	public ServerClient(int port, int nbrOfThreads) throws IOException {
+		pool = new RunOnThreadN(nbrOfThreads);
+		serverSocket = new ServerSocket(port);
+		pool.start();
+		connection.start();
+	}
+
+	private class Connection extends Thread {
+		public void run() {
+			System.out.println("ServerF running, port: " + serverSocket.getLocalPort());
+			while(true) {
+				try  {
+					Socket socket = serverSocket.accept();
+					pool.execute(new ClientHandler(socket));
+				} catch(IOException e) { 
+					System.err.println(e);
+				}
+			}
 		}
-		start();
 	}
-	
-	public void run() {
-		
+
+	private class ClientHandler implements Runnable {
+		private Socket socket;
+
+		public ClientHandler(Socket socket) {
+			this.socket = socket;
+		}
+
+		public void run() {
+			System.out.println("Klient uppkopplad, servas av " + Thread.currentThread());
+			try (ObjectOutputStream dos = new ObjectOutputStream(socket.getOutputStream());
+					ObjectInputStream dis = new ObjectInputStream(socket.getInputStream())	) {
+				
+				
+				ticket = dis.readUTF();
+				response = getResponse(ticket);
+				dos.writeUTF(response);
+				dos.flush();
+			} catch(IOException e) {}
+			try {
+				socket.close();
+			} catch(Exception e) {}
+			System.out.println("Klient nerkopplad, " + Thread.currentThread() + " återvänder till buffert");
+		}
 	}
-	
-	private class Worker {
-		
+
+	public static void main(String[] args) throws IOException {
+		new LotteryServerF(3465,50);
 	}
 }
